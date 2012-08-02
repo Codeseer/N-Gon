7a3c4dbbfce395d22a20cbe9091eeb0622a1391a
Scott Adams <scottadams@scottadams.me>
2012-08-02 18:24:06 -0400

    added user updating
    -ability to update users
    -removed node-modules folder from repository because that is system
    specific
    -organized code so it was more DRY

--- ngon-user/controllers/users.js
+++ ngon-user/controllers/users.js
@@ -3,65 +3,68 @@ var Game = require('../models/game.js');
 var User = require('../models/user.js');
 
 exports.create = function(req, res) {
-	console.log('--------');
-	//var json = Object.keys(req.body);
-	json = req.body;
-
-	console.log("creating new user");
 	var newUser = new User();
 
-	if(json.username)
-	{
-		newUser.username = json.username;
-		console.log("username = "+json.username);
+	if(req.body.username){
+		newUser.username = req.body.username;
 	}
-	if(json.first && json.last)
-	{
-		newUser.name.first = json.first;
-		newUser.name.last = json.last;
-		console.log('name = '+ json.first +' '+ json.last);
+	if(req.body.name){
+		newUser.name.first = req.body.name.first;
+		newUser.name.last = req.body.name.last;
 	}
-	if(json.email)
-	{
-		newUser.email = json.email;
-		console.log('email ='+ json.email);
+	if(req.body.email){
+		newUser.email = req.body.email;
 	}
 
-	newUser.save(function (err)
-	{
+	newUser.save(function (err){
 		if(!err)
 			res.send(newUser._id);
 		else
 			res.send(err);
 	});
-	
-	console.log("_id = "+newUser._id);
-	console.log('--------');
 }
 
 exports.show = function(req, res) {
-	if(req.params.id)
-	{		
-		User.findById(req.params.id, function(err, doc){
-			res.send(JSON.stringify(doc));
-		});
-	}
-	else if(req.params.username)
-	{
-		User.findOne({username: req.params.username}, function(err, doc){
-			res.send(JSON.stringify(doc));
-		});
-	}
-	else if(req.params.email)
-	{
-		User.findOne({email: req.params.email}, function(err, doc){
-			res.send(JSON.stringify(doc));
-		});
+
+	findUser(req.params, showUser);
+
+	function showUser(err, doc){
+		res.send(JSON.stringify(doc));
 	}
 }
 
 exports.update = function(req, res) {
+
+	findUser(req.params, updateUser);
+
+	function updateUser(err, doc){
+		if (req.body.username){			
+			doc.username = req.body.username;
+		}
+		if(req.body.email){
+			doc.email = req.body.email;			
+		}
+		if(req.body.name.first){
+			doc.name.first = req.body.name.first;			
+		}
+		if(req.body.name.last){
+			doc.name.last = req.body.name.last;
+		}
+	}
 }
 
 exports.delete = function(req, res) {
+}
+
+function findUser(params, callback)
+{
+	if(params.id){
+		User.findById(params.id, callback);
+	}
+	else if(params.username){
+		User.findOne({username: params.username}, callback);		
+	}
+	else if(params.email){
+		User.findOne({email: params.email}, callback);		
+	}
 }
